827c7d3a25d0aac50f59a68fd83ef684105d2176
Oleh Yastreba <ol.yastr17@gmail.com>
2025-10-02 22:38:45 +0300

    End of section 10

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from "src/app/app-routing.module";
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
@@ -7,7 +9,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 @NgModule({
     declarations: [NavBarComponent],
     imports: [
-        CommonModule
+        CommonModule,
+        AppRoutingModule,
+        RouterModule
     ],
     exports: [NavBarComponent]
 })
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from './shop.component';
 
 
@@ -9,12 +11,13 @@ import { ShopComponent } from './shop.component';
 @NgModule({
     declarations: [
         ShopComponent,
-        ProductItemComponent
+        ProductItemComponent,
+        ProductDetailsComponent
     ],
     imports: [
         CommonModule,
-        SharedModule
-    ],
-    exports: [ShopComponent]
+        SharedModule,
+        ShopRoutingModule
+    ]
 })
 export class ShopModule { }
